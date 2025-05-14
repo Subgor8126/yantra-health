834a0ae90ecbf1d66c253700ed55712da4fcb988
Sudhanshu <subgor8126@gmail.com>
2025-05-14 18:06:53 +0530

    auth: Apparently the authority is probably supposed to be the one ChatGPT said, the one with /oauth2 at the end, instead of the AWS Console authority

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_QAGkAfsHK",
+  authority: `https://${import.meta.env.VITE_COGNITO_DOMAIN}/oauth2`,
   client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
   redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
   response_type: import.meta.env.VITE_COGNITO_RESPONSE_TYPE,
